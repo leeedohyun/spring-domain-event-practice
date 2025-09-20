2dba89a37f1287110026318ba5befcf168dc31de
leeedohyun <ldh19738@gmail.com>
2025-09-20 14:09:22 +0900

    feat: 프로젝트 셋팅

---
+++ src/main/java/com/example/domain_event_practice/DomainEventPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.domain_event_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainEventPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainEventPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/domain_event_practice/DomainEventPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domain_event_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainEventPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
